12e9b46758ee4e7aa1f18a37c7be15579f7785d3
Harsh88-cmd <harshx8878@gmail.com>
2025-05-05 03:42:00 +0530

    Update app.js

--- app.js
+++ app.js
@@ -82,14 +82,16 @@ passport.use(new LocalStrategy(User.authenticate()))
 passport.serializeUser(User.serializeUser())
 passport.deserializeUser(User.deserializeUser())
 
-app.get('/favicon.ico', (req, res) => res.status(204));
-
 app.use((req,res,next)=>{
     res.locals.success = req.flash("success")
     res.locals.error = req.flash("error")
     res.locals.currUser = req.user || null
     next()
 })
+
+
+app.get('/favicon.ico', (req, res) => res.status(204));
+
 
 app.use("/listings", listingRouter)
 app.use("/listings/:id/reviews", reviewRouter)
